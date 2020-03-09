40b0390c2fb373f3f18ad0f2f540e7a657542b7d
Carlos Guillot <c.guillot@outlook.com>
2020-03-09 08:38:18 -0600

    Added hero panel component and styles

---
+++ src/app/hero-panel/hero-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-panel',
+  templateUrl: './hero-panel.component.html',
+  styleUrls: ['./hero-panel.component.css']
+})
+export class HeroPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
