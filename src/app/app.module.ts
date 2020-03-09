f25b3fd1a9039e6b1417c14fb5caf8aeada2aeba
Carlos Guillot <c.guillot@outlook.com>
2020-03-09 08:39:04 -0600

    Added components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroPanelComponent } from './hero-panel/hero-panel.component';
+import { ButtonPanelComponent } from './button-panel/button-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    HeroPanelComponent,
+    ButtonPanelComponent
   ],
   imports: [
     BrowserModule
