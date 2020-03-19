443ec70a0cfa32e812dc357f267b3fbdbb3f7425
Carlos Guillot <c.guillot@outlook.com>
2020-03-18 21:55:21 -0600

    Added initial Hero variables and passed them to the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'technogi-exercise';
+    heroTitle = 'Angular Demo'
+    heroSubtitle = 'with 💔 by CGuillot'
 }
