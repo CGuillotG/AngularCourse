ddb5f9b375f006d091dd6ceb1d94db5717ec1880
Carlos Guillot <c.guillot@outlook.com>
2020-03-20 14:41:33 -0600

    Deploy (2020-03-20_14:40)

--- src/app/armory/armory.component.ts
+++ src/app/armory/armory.component.ts
@@ -10,7 +10,7 @@ export class ArmoryComponent implements OnInit {
     weapons:Array<Weapon> = [
         new Weapon('Patron of Lost Causes', 'Scout Rifle', 'Kinetic','https://bungie.net/common/destiny2_content/icons/9d0a1ad5374c23edd4454a0f41c38e89.jpg', 'https://bungie.net/common/destiny2_content/screenshots/2723241847.jpg',1500,10),
         new Weapon("Martyr's Retribution", 'Grenade Launcher', 'Energy','https://bungie.net/common/destiny2_content/icons/f9147f060f536f2c794bc3acc5100b55.jpg','https://bungie.net/common/destiny2_content/screenshots/3850168899.jpg',1500,10),
-        new Weapon("The Colony", 'Grenade Launcher', 'Heavy','https://bungie.net/common/destiny2_content/icons/dadeb1f4484ffdaec2420240700dc637.jpg','https://bungie.net/common/destiny2_content/screenshots/3899270607.jpg',0,0)
+        new Weapon("The Colony", 'Grenade Launcher', 'Heavy','https://bungie.net/common/destiny2_content/icons/dadeb1f4484ffdaec2420240700dc637.jpg','https://bungie.net/common/destiny2_content/screenshots/3899270607.jpg',2000,50)
     ]
     selectedWeapon:Weapon
     cart:Array<Weapon> = []
