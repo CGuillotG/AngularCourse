af0ef8bf9d0fdc38b637f2539d813063a23079ce
Carlos Guillot <c.guillot@outlook.com>
2020-03-27 23:13:11 -0600

    Renamed main folder

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('technogi-exercise app is running!');
+    expect(page.getTitleText()).toEqual('spacebar-exercise app is running!');
   });
 
   afterEach(async () => {
