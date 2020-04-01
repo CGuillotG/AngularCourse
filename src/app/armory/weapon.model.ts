75e61bacc78652a2ab0f692b95ae09973e840002
Carlos Guillot <c.guillot@outlook.com>
2020-04-01 14:24:25 -0600

    Made weapon model more compact

--- src/app/armory/weapon.model.ts
+++ src/app/armory/weapon.model.ts
@@ -1,19 +1,11 @@
 export class Weapon {
-    public name:string
-    public type:string
-    public slot:string
-    public icon: string
-    public screenshot: string
-    public glimmer:number
-    public shards:number
-
-    constructor(name:string,type:string, slot:string, icon:string, screenshot:string, glimmer:number, shards:number) {
-        this.name = name
-        this.type = type
-        this.slot = slot
-        this.icon = icon
-        this.screenshot = screenshot
-        this.glimmer = glimmer
-        this.shards = shards
-    }
+    constructor(
+        public name: string,
+        public type: string,
+        public slot: string,
+        public icon: string,
+        public screenshot: string,
+        public glimmer: number,
+        public shards: number
+        ) { }
 }
