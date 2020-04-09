eda57a88b581157f2f6bfe906db8b93b9e48c01b
Carlos Guillot <c.guillot@outlook.com>
2020-04-08 21:57:02 -0500

    Added subscription to getCharacterIds from service

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { D2CharactersService } from '../services/d2-characters.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-characters',
-  templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.css']
+    selector: 'app-characters',
+    templateUrl: './characters.component.html',
+    styleUrls: ['./characters.component.css']
 })
-export class CharactersComponent implements OnInit {
+export class CharactersComponent implements OnInit, OnDestroy {
+    
+    private d2CharacterIdsSubscription: Subscription
+    
+    characterIds:string[] = []
 
-  constructor() { }
+    constructor(private d2CharactersService: D2CharactersService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.d2CharacterIdsSubscription = this.d2CharactersService.getCharacterIds().subscribe(
+            response => {
+                this.characterIds = response
+                console.log(this.characterIds)
+            },
+            error => console.error('Error caught:',error))
+    }
+    ngOnDestroy(): void {
+        this.d2CharacterIdsSubscription.unsubscribe()
+    }
 
 }
